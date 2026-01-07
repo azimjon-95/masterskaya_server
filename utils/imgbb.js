c269ea8268d0d2c21de102f26b31ad3a64b28cbc
azimjon-95 <azimjon995m@gmail.com>
2026-01-07 07:47:43 +0500

    Statistika img

--- utils/imgbb.js
+++ utils/imgbb.js
@@ -11,14 +11,69 @@ export const uploadToImgBB = async (filePath) => {
             new URLSearchParams({
                 key: process.env.IMGBB_API_KEY,
                 image: base64,
-            })
+            }),
+            {
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                },
+            }
         );
 
         return res.data.data.url;
+
+    } catch (err) {
+        console.error("ImgBB upload error:", err?.response?.data || err.message);
+        throw err;
     } finally {
-        // vaqtinchalik faylni o‘chiramiz
+        // 🔥 HAR DOIM uploads dan o‘chadi
         if (fs.existsSync(filePath)) {
             fs.unlinkSync(filePath);
         }
     }
 };
+
+
+
+
+
+
+
+
+
+
+// import fs from "fs";
+// import axios from "axios";
+
+// export const uploadToImgBB = async (filePath) => {
+//     try {
+//         const buffer = fs.readFileSync(filePath);
+//         const base64 = buffer.toString("base64");
+
+//         // const res = await axios.post(
+//         //     "https://api.imgbb.com/1/upload",
+//         //     new URLSearchParams({
+//         //         key: process.env.IMGBB_API_KEY,
+//         //         image: base64,
+//         //     })
+//         // );
+//         const res = await axios.post(
+//             "https://api.imgbb.com/1/upload",
+//             new URLSearchParams({
+//                 key: process.env.IMGBB_API_KEY,
+//                 image: base64,
+//             }),
+//             {
+//                 headers: {
+//                     "Content-Type": "application/x-www-form-urlencoded",
+//                 },
+//             }
+//         );
+
+//         return res.data.data.url;
+//     } finally {
+//         // vaqtinchalik faylni o‘chiramiz
+//         if (fs.existsSync(filePath)) {
+//             fs.unlinkSync(filePath);
+//         }
+//     }
+// };
--- utils/multer.js
+++ utils/multer.js
@@ -4,7 +4,7 @@ import fs from "fs";
 
 const uploadDir = "uploads";
 if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
+    fs.mkdirSync(uploadDir, { recursive: true });
 }
 
 const storage = multer.diskStorage({
